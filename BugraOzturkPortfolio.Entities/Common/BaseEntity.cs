8fa67117d31a1057a545444c85a6fd6e34767b2c
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-19 22:02:23 +0300

    feat: integrate RT.Comb for sequential Guid generation in BaseEntity

---
+++ BugraOzturkPortfolio.Entities/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using RT.Comb;
+
+namespace BugraOzturkPortfolio.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Provider.Sql.Create();
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
