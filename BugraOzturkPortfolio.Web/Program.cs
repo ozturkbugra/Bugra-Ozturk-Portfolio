5996d5f4d12fd4dba5aa1e0f6c97af7781daebab
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-20 21:45:15 +0300

    chore: register generic repository and unit of work to DI container in Program.cs

--- BugraOzturkPortfolio.Web/Program.cs
+++ BugraOzturkPortfolio.Web/Program.cs
@@ -1,4 +1,6 @@
 using BugraOzturkPortfolio.DataAccess.Context;
+using BugraOzturkPortfolio.DataAccess.Repositories.Abstract;
+using BugraOzturkPortfolio.DataAccess.Repositories.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
