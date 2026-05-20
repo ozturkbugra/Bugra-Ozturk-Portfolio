a360d944013ebc40b5c3ed4a6ad7c1acdcb5c9d4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-20 22:05:12 +0300

    feat: setup Admin area with DashboardController and configure area routing in Program.cs

---
+++ BugraOzturkPortfolio.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugraOzturkPortfolio.DataAccess.Repositories.Abstract;
+using BugraOzturkPortfolio.Entities.Concrete;
+
+namespace BugraOzturkPortfolio.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalProjects = await _unitOfWork.GetRepository<Project>().CountAsync();
+            ViewBag.TotalProjects = totalProjects;
+
+            return View();
+        }
+    }
+}
