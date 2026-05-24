9995ab4a32abce6a6097f04f6651be3b5618c097
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-24 19:13:06 +0300

    feat: integrate SkillsViewComponent

---
+++ BugraOzturkPortfolio.Web/ViewComponents/SkillsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugraOzturkPortfolio.Business.Abstract;
+
+namespace BugraOzturkPortfolio.Web.ViewComponents
+{
+    public class SkillsViewComponent : ViewComponent
+    {
+        private readonly ISkillService _skillService;
+
+        public SkillsViewComponent(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var skills = await _skillService.GetAllSkillsAsync();
+
+            var groupedSkills = skills
+                .Where(s => !s.IsDeleted)
+                .OrderBy(s => s.Order)
+                .GroupBy(s => s.GroupName)
+                .ToList();
+
+            return View(groupedSkills);
+        }
+    }
+}
