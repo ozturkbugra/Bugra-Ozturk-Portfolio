a369790baf4863ba8a7f94c8fc5ee7c1d04aa9b7
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-24 22:58:01 +0300

    feat: implement TestimonialsViewComponent with dynamic grid layout detection

---
+++ BugraOzturkPortfolio.Web/ViewComponents/TestimonialsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugraOzturkPortfolio.Business.Abstract;
+
+namespace BugraOzturkPortfolio.Web.ViewComponents
+{
+    public class TestimonialsViewComponent : ViewComponent
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialsViewComponent(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var testimonials = await _testimonialService.GetAllTestimonialsAsync();
+
+            var activeTestimonials = testimonials
+                .Where(t => !t.IsDeleted)
+                .OrderBy(t => t.Order)
+                .ToList();
+
+            return View(activeTestimonials);
+        }
+    }
+}
