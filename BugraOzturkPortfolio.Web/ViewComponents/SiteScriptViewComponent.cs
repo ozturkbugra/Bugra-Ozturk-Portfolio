c5ac129e9949d1be2668125ef4415576f4df9aad
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-24 18:55:00 +0300

    feat: integrate SiteScriptViewComponent with enum positions on layout

---
+++ BugraOzturkPortfolio.Web/ViewComponents/SiteScriptViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugraOzturkPortfolio.Business.Abstract;
+using BugraOzturkPortfolio.Entities.Concrete;
+
+namespace BugraOzturkPortfolio.Web.ViewComponents
+{
+    public class SiteScriptViewComponent : ViewComponent
+    {
+        private readonly ISiteScriptService _siteScriptService;
+
+        public SiteScriptViewComponent(ISiteScriptService siteScriptService)
+        {
+            _siteScriptService = siteScriptService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ScriptPosition position)
+        {
+            var scripts = await _siteScriptService.GetActiveScriptsByPositionAsync(position);
+            return View(scripts);
+        }
+    }
+}
