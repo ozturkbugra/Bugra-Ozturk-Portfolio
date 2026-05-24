0c6f0ea33ecdab6ae9a7c98e2de6bc5ec01fe59c
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-24 22:53:10 +0300

    feat: implement ServicesViewComponent with dynamic title accent parsing

---
+++ BugraOzturkPortfolio.Web/ViewComponents/ServicesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugraOzturkPortfolio.Business.Abstract;
+
+namespace BugraOzturkPortfolio.Web.ViewComponents
+{
+    public class ServicesViewComponent : ViewComponent
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServicesViewComponent(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var services = await _serviceService.GetAllServicesAsync();
+
+            var activeServices = services
+                .Where(s => !s.IsDeleted)
+                .OrderBy(s => s.Order)
+                .ToList();
+
+            return View(activeServices);
+        }
+    }
+}
