5a627073a41ac05414d819fc3a5754086a211a50
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-25 21:05:10 +0300

    feat(web): migrate layout scripts into LayoutScriptViewComponent for cleaner modularity

---
+++ BugraOzturkPortfolio.Web/ViewComponents/LayoutScriptViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugraOzturkPortfolio.Web.ViewComponents
+{
+    public class LayoutScriptViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
